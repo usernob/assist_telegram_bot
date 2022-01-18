d76b7ef118b4b89bfa7bc9121aaf889f2207b8d3
Usernob <ramzipandu2019@gmail.com>
2022-01-18 17:19:38 +0700

    fixed

--- db.py
+++ db.py
@@ -2,7 +2,6 @@ import sqlite3
 
 def make_table():
     connection = sqlite3.connect('main.db')
-    connection.executescript('DROP TABLE links')
     connection.executescript('CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY AUTOINCREMENT,original_url TEXT NOT NULL, mime_type TEXT NOT NULL)')
     
     connection.commit()
