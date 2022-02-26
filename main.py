aa1f3e3808a589903f62a69287c63b600b95113d
Usernob <ramzipandu2019@gmail.com>
2022-02-26 22:36:17 +0700

    firstcommit

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ with bot:
 async def start(bot,msg):
     print(msg)
     await msg.reply(
-        'hello what can i help you?',
+        '\U0001f600 hello what can i help you?',
         reply_markup = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton(
